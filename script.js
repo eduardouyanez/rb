e2749521284f97470cd2ac81584b214dd9d46eee
eduardouyanez <57239705+eduardouyanez@users.noreply.github.com>
2025-01-01 20:03:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const header = document.getElementById('header');
 const menu = document.getElementById('menu');
 const topRightMenu = document.getElementById('top-right-menu');
 const navbar = document.getElementById('navbar');
+const menuToggle = document.getElementById('menu-toggle');
+const docentesMenu = document.getElementById('docentes-menu');
+const docentesSubmenu = document.getElementById('docentes-submenu');
 const headerHeight = header.offsetHeight;
 
 // Cambiar fondo del header y menú al hacer scroll
@@ -17,26 +20,27 @@ window.addEventListener('scroll', () => {
 });
 
 // Menú toggle para pantallas pequeñas
-const menuToggle = document.getElementById('menu-toggle');
-
 menuToggle.addEventListener('click', () => {
   menu.classList.toggle('hidden');
   menu.classList.toggle('show');
 });
 
-const docentesMenu = document.getElementById('docentes-menu');
-const docentesSubmenu = document.getElementById('docentes-submenu');
-
-// Muestra u oculta el submenú al hacer clic en "Docentes"
+// Mostrar u ocultar el submenú de Docentes
 docentesMenu.addEventListener('click', (e) => {
   e.preventDefault();
-  docentesSubmenu.classList.toggle('hidden');
-});
-
-// Oculta el submenú al hacer clic fuera de él
-document.addEventListener('click', (e) => {
-  if (!docentesMenu.contains(e.target) && !docentesSubmenu.contains(e.target)) {
+  if (docentesSubmenu.classList.contains('hidden')) {
+    docentesSubmenu.classList.remove('hidden');
+    docentesSubmenu.classList.add('visible');
+  } else {
+    docentesSubmenu.classList.remove('visible');
     docentesSubmenu.classList.add('hidden');
   }
 });
 
+// Ocultar el submenú si se hace clic fuera
+document.addEventListener('click', (e) => {
+  if (!docentesMenu.contains(e.target) && !docentesSubmenu.contains(e.target)) {
+    docentesSubmenu.classList.add('hidden');
+    docentesSubmenu.classList.remove('visible');
+  }
+});
